304cc9a16692e9a336c0786c11eefc2c9718dfcd
tjwhdwn <tjwhdwn@naver.com>
2023-09-25 18:49:59 +0900

    리액트 연결되는 라우터 추가 /join /login

--- src/main/frontend/client/src/App.tsx
+++ src/main/frontend/client/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
+import { Route, Routes } from "react-router-dom";
+import Join from "./join/join";
+import Login from "./join/login";
+
 function App() {
   const [ message, setMessage ] = useState([]);
 
@@ -11,7 +15,10 @@ function App() {
 
   return (
       <div className="App">
-
+            <Routes>
+                  <Route path="/join" element={<Join />} />
+                  <Route path="/login" element={<Login />} />
+            </Routes>
       </div>
   );
 }
