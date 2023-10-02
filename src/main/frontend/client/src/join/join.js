07c6cc0602cd984bdf99a7a3876a07f1f5ed2204
tjwhdwn <tjwhdwn@naver.com>
2023-10-03 01:04:54 +0900

    회원가입 데이터 테스트

--- src/main/frontend/client/src/join/join.js
+++ src/main/frontend/client/src/join/join.js
@@ -1,5 +1,6 @@
-import React, { useState, createContext, useContext, Component } from 'react';
-import {Wrap, Inner, Input, EmailInput,  FlexSt} from './styles/emotion'
+import React, { useState, createContext, useContext, Component, useEffeact } from 'react';
+import {Wrap, Inner, Input, EmailInput,  FlexSt} from './styles/emotion';
+import axios from "axios";
 
 export default function Join(){
         const [id, setId] = useState('');
@@ -8,37 +9,48 @@ export default function Join(){
         const [email1, setEmail1] = useState('');
         const [email2, setEmail2] = useState('');
         const [phone, setPhone] = useState('');
-    
-        const [domainList, setDomainList] = useState([
-           {title:'네이버', value:'naver.com'},
-           {title:'구글', value:'google.com'},
-           {title:'한메일', value:'hanmail'},
-           {title:'네이트', value:'nate.com'},
-           {title:'카카오', value:'kakao.com'}
-        ]);
-        const [phoneList, setPhoneList] =  ([
-           {title:'010', value:'010'},
-           {title:'011', value:'011'},
-           {title:'016', value:'016'},
-           {title:'017', value:'017'},
-           {title:'019', value:'019'}
-        ]);
-    function SelectList(props){
-       const opt = []
-       opt.push( <option value="">선택없음</option> )
-       for(let i=0; i < props.list.length ; i++){
-         let t = props.list[i];
-         opt.push(
-           <option id={t.id} value={t.value}>{t.title}</option>
-         )
-       }
-       return <select id={props.id} onChange={ (e) => { setEmail2(e.target.value);} }>{opt}</select>
+
+    /*
+    useEffeact( () => {
+        axios.("https://localhost:9090/api/member/join",{
+            userId : id,
+            userPw : pw,
+            userEmail : email1."@".emotion,
+        })
+        .then( (response) => {
+            console.log(response);
+        })
+        .catch( (error) => {
+            console.log(error);
+        })
+    })
+    */
+
+    const dataSubmit = (e) => {
+        const formData = new FormData();
+        console.log(id);
+        console.log(pw);
+        console.log(email1+'@'+email2);
+        formData.append('id', id);
+        formData.append('pw', pw);
+        formData.append('email',email1+'@'+email2);
+
+
+        axios({
+          method:'post',
+          url:'localhost:9090/api/member/join',
+          data: formData,
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
     }
+
     return(
         <Wrap>
             <Inner>
                 <a href='#;' style={{textDecoration:"none",color:"#999",fontSize:"12px"}}>이전 페이지</a>
-
                 <p>정보입력</p>
                 <div>
                     <Input>
@@ -54,26 +66,37 @@ export default function Join(){
                     <Input>
                         <p>비밀번호 다시 입력해주세요.</p>
                         <EmailInput type="password" value={pwchk} placeholder='입력하신 비밀번호와 동일하게 입력해 주세요.'
-                            onChange={ (e) => {pwchk(e.target.value)} }/>
+                            onChange={ (e) => {setPwChk(e.target.value)} }/>
                         <p style={{color:"#FF0000",fontSize:"12px"}}> 동일한 비밀번호를 입력해주세요. </p>
                     </Input>
                     <Input>
                         <p>이메일을 입력해주세요.</p>
                         <FlexSt>
-                            <EmailInput type="text"/>
+                            <EmailInput type="text" />
                             @
                             <EmailInput type="text" value={email2}/>
-                            <SelectList id="domain-list" list={domainList} />
+                            <select id="domain-list" onChange={(e) =>{setEmail2(e.target.value)} }>
+                                <option value="naver.com">naver.com</option>
+                                <option value="google.com">google.com</option>
+                                <option value="hanmail.net">hanmail.net</option>
+                                <option value="nate.com">nate.com</option>
+                                <option value="kakao.com">kakao.com</option>
+                            </select>
+
                         </FlexSt>
                     </Input>
                     <Input>
                         <p>핸드폰 번호 입력해주세요.</p>
                         <FlexSt>
-                            <SelectList id="phone-list" list={phoneList}/>
+                            <select id="phone-list">
+                                <option>010</option>
+                                <option>011</option>
+                            </select>
                             <EmailInput type="text" placeholder="‘-’ 없이 입력해주세요." maxLength={8}/>
                             <a href='#;' onClick={(e)=>{
                                e.preventDefault();
                                alert("인증번호 발송");
+                               dataSubmit();
                             }}>인증번호 발송</a>
                         </FlexSt>
                     </Input>
