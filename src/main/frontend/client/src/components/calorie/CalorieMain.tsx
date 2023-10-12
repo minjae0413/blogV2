abd4e584861725076b36b25cb67aa4b068ac1d01
[웹개발2팀]위건 <juhyeon2398@naver.com>
2023-10-12 17:58:18 +0900

    FEAT : 칼로리 API 테스트

--- src/main/frontend/client/src/components/calorie/CalorieMain.tsx
+++ src/main/frontend/client/src/components/calorie/CalorieMain.tsx
@@ -2,29 +2,58 @@ import {useState,useEffect} from "react";
 import axios from "axios";
 
 const CalorieMain = () =>{
-  
     const apiKey= process.env.REACT_APP_CALORIE_KEY; // remove this after you've confirmed it is working
-    const [code, setCode] = useState('');
-    const [msg, setMsg] = useState('');
-    useEffect( () => {
-        axios.get(`https://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?serviceKey=LuzMRse%2BHSLpVAku6AHXRloynZXPkYnXBtQ295lZXRdwo5y%2FSJCRsQ7GErzxJTcTFUMMJRrFMOWPjyu7dcF0JQ%3D%3D&desc_kor=%EB%B0%94%EB%82%98%EB%82%98%EC%B9%A9&pageNo=1&numOfRows=3&bgn_year=2017&animal_plant=(%EC%9C%A0)%EB%8F%8C%EC%BD%94%EB%A6%AC%EC%95%84&type=XML`,{
-        // axios.get(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=LuzMRse%2BHSLpVAku6AHXRloynZXPkYnXBtQ295lZXRdwo5y%2FSJCRsQ7GErzxJTcTFUMMJRrFMOWPjyu7dcF0JQ%3D%3D&numOfRows=10&pageNo=1&base_date=20231004&dataType=JSON&base_time=0600&nx=55&ny=127`,{
+  
+    const [foodInfo, setFood] = useState({
+        food:"",
+        kcal:0,
+    });
+
+    function todayFood(){
+        axios.get(`https://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?serviceKey=${apiKey}&desc_kor=${foodInfo.food}&pageNo=1&numOfRows=3&type=json`,{
         })
         .then( (header) => {
             const jsonData = header.data;
-            const returnval = jsonData['header'];
-            setCode(returnval['resultCode']);
-            setMsg(returnval['resultMsg']);
+            const returnval = jsonData['body']['items'];
+            returnval.forEach((el:any,idx:Number)=> {
+                console.log("    el:", el)
+                console.log("    foodInfo:", foodInfo)
+            });
+          
         })
-        .catch( (error) => {
+         .catch( (error) => {
             console.log(error);
         })
-    }, []);
+        
+    }
+ 
+
+    const todayFoodState = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFood({
+            ...foodInfo,
+            [e.target.name] : e.target.value,
+        })
+    }
+    
+    const todayFoodSubmit = () => {
+        todayFood()
+    }
 
     return (
         <article>
-            <p>{code}</p>
-            <p>{msg}</p>
+            <ul>
+                <li>
+                    <label htmlFor="">오늘 먹은 음식 : </label>
+                    <input type="text" value={foodInfo.food} name="food" onChange={todayFoodState}/>
+                    <button onClick={todayFoodSubmit}>입력</button>
+                </li>
+            </ul>
+            
+            <div>
+                <select name="" id="">
+                    
+                </select>
+            </div>
         </article>
     );
 }
