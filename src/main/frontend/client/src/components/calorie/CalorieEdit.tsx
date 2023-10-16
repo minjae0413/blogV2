0d211671b1500f9353edb2a2d02a53a0aeedca9d
[웹개발2팀]위건 <juhyeon2398@naver.com>
2023-10-16 17:59:52 +0900

    FEAT : 칼로리 API 검색데이터 필터링 테스트

--- src/main/frontend/client/src/components/calorie/CalorieEdit.tsx
+++ src/main/frontend/client/src/components/calorie/CalorieEdit.tsx
@@ -1,6 +1,22 @@
 import {useState,useEffect} from "react";
 import axios from "axios";
 
+interface Types {
+    ANIMAL_PLANT: string,
+    BGN_YEAR: string,
+    DESC_KOR: string,
+    NUTR_CONT1: string,
+    NUTR_CONT2: string,
+    NUTR_CONT3: string,
+    NUTR_CONT4: string,
+    NUTR_CONT5: string,
+    NUTR_CONT6: string,
+    NUTR_CONT7: string,
+    NUTR_CONT8: string,
+    NUTR_CONT9: string,
+    SERVING_WT: string,
+}
+
 const CalorieEdit = () =>{
     const apiKey= process.env.REACT_APP_CALORIE_KEY; // remove this after you've confirmed it is working
   
@@ -17,11 +33,25 @@ const CalorieEdit = () =>{
         })
         .then( (header) => {
             const jsonData = header.data;
-            const returnval = jsonData['body']['items'];
-            returnval.forEach((el:string, idx:number) => {
-                foodOption += `<option value=${returnval[idx]['NUTR_CONT1']}>${returnval[idx]['DESC_KOR']}</option>`
-            });
-            setOptions(foodOption)
+            let returnval = jsonData['body']['items'];
+            console.log("   before returnval:", returnval)
+            returnval = returnval.filter((el:Types, idx:number) => {
+                if(idx >= 1 ){
+                    if(el['DESC_KOR'] == returnval[idx-1]['DESC_KOR']){
+    
+                        console.log("    returnval[idx-1]:", returnval[idx-1])
+                        return (parseInt(el['BGN_YEAR']) <= parseInt(returnval[idx-1]['BGN_YEAR'])) ? el : returnval[idx-1];
+                    }
+                }
+            })
+            console.log("    returnval:", returnval)
+            // returnval.forEach((el:Types, idx:number) => {
+            //     console.log("    returnval:", returnval[idx]['DESC_KOR'], returnval[idx]['BGN_YEAR'])
+            //     if(idx >= 1 && el['DESC_KOR'] != returnval[idx-1]['DESC_KOR']){
+            //         foodOption += `<option value=${returnval[idx-1]['NUTR_CONT1']} data-txt=${returnval[idx-1]['SERVING_WT']}>${returnval[idx-1]['DESC_KOR']}${returnval[idx-1]['BGN_YEAR']}</option>`
+            //     }
+            // });
+            // setOptions(foodOption)
         })
          .catch( (error) => {
             console.log(error);
@@ -42,11 +72,8 @@ const CalorieEdit = () =>{
     }
 
     // 
-    const foodInfohandle = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFood({
-            ...foodInfo,
-            [e.target.name] : e.target.value,
-        })
+    const foodInfohandle = () => {
+      
     }
 
     return (
@@ -60,7 +87,7 @@ const CalorieEdit = () =>{
             </ul>
             
             <div>
-                <select name="" id="FoodList" dangerouslySetInnerHTML={{ __html: options }}>
+                <select name="" id="FoodList" dangerouslySetInnerHTML={{ __html: options }} onChange={foodInfohandle}>
                 </select>
             </div>
         </article>
